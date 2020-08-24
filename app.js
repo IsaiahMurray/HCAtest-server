bc9afd1dd468ecebd65097d3a0e7b14d1c411089
Isaiah Murray <murray.isaiah13@gmail.com>
2020-08-24 16:15:01 -0400

    Separating models and controllers from app.js

--- app.js
+++ app.js
@@ -5,57 +5,70 @@ const { Sequelize, DataTypes } = require("sequelize");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const validateSession = require("./middleware/validate-session");
 require("dotenv").config();
+
+let user = require("./controllers/usercontroller");
 
 const app = express();
 const db = new Sequelize(
   "postgres://postgres:weewoo123@localhost:5432/hca_database"
 );
 
-// let user = require('./controllers/usercontroller');
-//---------------USER MODEL-----------------
-const User = db.define(
-  "User",
-  {
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    isAdmin: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-  },
-  {}
-);
+/**********************************
+ ********   ITEM MODEL   *********
+ *********************************/
+//commented out item stuff to focus on getting the user to work first
 
-const Item = db.define(
-  "Item",
-  {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.STRING
-    },
-    rarity: {
-      type: DataTypes.INTEGER
-    },
-    value: {
-      type: DataTypes.INTEGER
-    },
-    itemId: {
-      type: DataTypes.INTEGER
-    }
-  },
-  {}
-);
+// const Item = db.define(
+//   "Item",
+//   {
+//     name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     description: {
+//       type: DataTypes.STRING,
+//     },
+//     rarity: {
+//       type: DataTypes.INTEGER,
+//     },
+//     value: {
+//       type: DataTypes.INTEGER,
+//     },
+//     itemId: {
+//       type: DataTypes.INTEGER,
+//     },
+//     amountNeeded: {
+//       type: DataTypes.INTEGER,
+//     },
+//   },
+//   {}
+// );
+
+/**********************************
+ ******** ITEM LIST MODEL *********
+ *********************************/
+
+// const ItemList = db.define(
+//   "ItemList",
+//   {
+//     name: {
+//       type: DataTypes.STRING,
+//     },
+//     owner: {
+//       type: DataTypes.INTEGER,
+//     },
+//   },
+//   {}
+// );
+
+// ItemList.hasMany(Item);
+// Item.belongsTo(ItemList, {
+//   foreignKey: {
+//     name: "itemId",
+//   },
+// });
 
 app.use(express.json());
 
@@ -63,73 +76,42 @@ app.get("/", (req, res) => {
   res.json({ message: "Working, base route" });
 });
 
-app.use(
-  "/user", // User controller here
+// app.use(
+//   "/item", // Item controller here
+//   router.post("/create", /*validateSession,*/  (req, res) => {
 
-  /**********************************
-   ********   USER CREATE   *********
-   *********************************/
+// /**********************************
+// ********   ITEM CREATE   *********
+// *********************************/
 
-  router.post("/create", function (req, res) {
-    User.create({
-      email: req.body.user.email,
-      password: bcrypt.hashSync(req.body.user.password, 13),
-    })
-      .then(function createSuccess(user) {
-        let token = jwt.sign(
-          { id: user.id, email: user.email },
-          process.env.JWT_SECRET,
-          { expiresIn: 60 * 60 * 24 }
-        );
+//     const item = {
+//       name: req.body.item.name,
+//       description: req.body.item.description,
+//       rarity: req.body.item.rarity,
+//       type: req.body.item.type,
+//       value: req.body.item.value,
+//       itemId: req.body.item.itemId,
+//       amountNeeded: req.body.item.amountNeeded,
+//     };
+//     Item.create(item)
+//       .then((item) => res.status(200).json(item))
+//       .catch((err) => res.status(500).json({ error: err }));
+//   }),
 
-        res.json({
-          user: user,
-          message: "User successfully created!",
-          sessionToken: token,
-        });
-      })
-      .catch((err) => log(chalk.redBright(err)));
-    //--------------------------------------
-  }),
-  /**********************************
-   ********   USER LOGIN   *********
-   *********************************/
+// /**********************************
+// ********  GET ALL ITEMS  **********
+// ***********************************/
 
-  router.post("/login", function (req, res) {
-    User.findOne({
-      where: {
-        email: req.body.user.email,
-      },
-    })
-      .then(function loginSuccess(user) {
-        if (user) {
-          bcrypt.compare(req.body.user.password, user.password, function (err, matches) {
-            if (matches) {
-              let token = jwt.sign(
-                { id: user.id},
-                process.env.JWT_SECRET,
-                { expiresIn: 60 * 60 * 24 })
+// router.get("/", (req, res) => {
+//   Item.findAll()
+//   .then(items => res.status(200).json(items))
+//   .catch(err => res.status(500).json({ error: err}))
+// })
+// );
 
-                res.status(200).json({
-                  user: user,
-                  message: "User has been logged in!",
-                  sessionToken: token
-              })
-            } else {
-              res.status(502).send({ error: "Login failed!! Who are you?!" });
-            }
-          });
-        } else {
-          res.status(500).json({ error: "User does not exist." });
-        }
-      })
-      .catch((err) => res.status(500).json({ error: err }));
-  })
-);
+app.use("/user", user);
 
-app.use("/item")
- 
-app.use(require('./middleware/validate-session'));
+app.use(require("./middleware/validate-session"));
 
 // Code down here is the startup procedure
 // 1 - Check DB Authentication
