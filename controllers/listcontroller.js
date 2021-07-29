219e09251c7635437a644015f836b83cf99de140
Isaiah Murray <murray.isaiah13@gmail.com>
2021-07-29 18:07:39 -0400

    updated get method

--- controllers/listcontroller.js
+++ controllers/listcontroller.js
@@ -1,28 +1,27 @@
 const router = require("express").Router();
-const {ListModel, TaskModel} = require("../models");
+const { ListModel, TaskModel } = require("../models");
 const chalk = require("chalk");
 
 router.get("/test", function (req, res) {
   res.send("Hey!! This is the list route!");
 });
 
-router.post("/create", async(req, res) => {
+router.post("/create", async (req, res) => {
+  const { title, description } = req.body;
+  const listEntry = {
+    title: title,
+    description: description,
+    owner: req.user.id,
+  };
 
-    const {title, description} = req.body;
-    const listEntry = {
-      title: title,
-      description: description,
-      owner: req.user.id
-    }
-
-  try{
+  try {
     const newList = await ListModel.create(listEntry);
 
     res.status(200).json({
       message: "New list has successfully been created!",
-      newList
-    })
-  } catch(err) {
+      newList,
+    });
+  } catch (err) {
     chalk.redBright(
       res.status(500).json({
         message: `List could not be created: ${err}`,
@@ -45,34 +44,27 @@ router.post("/create", async(req, res) => {
 //     .catch((err) => res.status(500).json({ error: err }));
 // });
 
-router.post("/", async(req, res) => {
-try{
-  const allLists = await ListModel.findAll({
-    where: { owner: req.user.id }
-  });
+router.post("/", async (req, res) => {
+  try {
+    const allLists = await ListModel.findAll({
+      where: { owner: req.user.id }
+    });
 
-  if(allLists.length === 0){
-    return res.status(204).json({
-      message: "You do not have any lists yet. Go make some!"
-    })
-  } else {
-    return res.status(200).json({
-      message: "Lists have successfully been retrieved",
-      allLists
-    })
-  }
-
-  res.status(200).json({
-    message: "All lists have successfully been retrieved!",
-    allLists
-  })
-} catch(err) {
-  
+    if (allLists.length === 0 || null) {
+      return res.status(204).json({
+        message: "You do not have any lists yet. Go make some!",
+      });
+    } else {
+      return res.status(200).json({
+        message: "Lists have successfully been retrieved",
+        allLists,
+      });
+    }
+  } catch (err) {
     res.status(500).json({
       message: `Lists could not be created: ${err}`,
-    })
-  
-}
+    });
+  }
 });
 
 router.get("/:title", (req, res) => {
@@ -90,7 +82,6 @@ router.get("/:title", (req, res) => {
     });
 });
 
-  
 router.put("/update/:id", (req, res) => {
   const list = {
     title: req.body.title,
@@ -100,9 +91,9 @@ router.put("/update/:id", (req, res) => {
   const query = { where: { id: req.params.id } };
 
   List.update(list, query)
-    .then(
-        
-        (list) => res.status(200).json({message: "Your list has been updated", list}))
+    .then((list) =>
+      res.status(200).json({ message: "Your list has been updated", list })
+    )
     .catch((err) => res.status(500).json({ error: err }));
 });
 
