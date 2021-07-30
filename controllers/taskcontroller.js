3d6f3f1dbb514aaa245cc5acebd25dcaac59d565
Isaiah Murray <murray.isaiah13@gmail.com>
2021-07-30 17:29:13 -0400

    updated task create

--- controllers/taskcontroller.js
+++ controllers/taskcontroller.js
@@ -1,42 +1,54 @@
 const router = require("express").Router();
-const { TaskModel } = require('../models');
+const { TaskModel } = require("../models");
 
-router.get('/test', function(req, res){
-    res.send('Hey!! This is the task route!')
+//!TEST ENDPOINT
+router.get("/test", function (req, res) {
+  res.send("Hey!! This is the task route!");
 });
 
-router.post('/create/:listId', (req, res) => {
-    const task = {
-        description: req.body.description,
-        owner: req.user.id,
-        listId: Number(req.params.listId)
-    }
-    TaskModel.create(task)
-    .then(task => res.status(200).json(task))
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({error: err})
-    })
+//! CREATE TASK
+router.post("/create/:listId", async (req, res) => {
+  const task = {
+    description: req.body.description,
+    owner: req.user.id,
+    listId: req.params.listId,
+  };
+  try {
+    const taskEntry = await TaskModel.create(task);
+
+    res.status(200).json({
+      message: "New task has successfully been created!",
+      taskEntry,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Task could not be created: ${err}`,
+    });
+  }
 });
 
-router.put('/update/:id', (req, res) => {
-    const task = {
-        description: req.body.description
-    };
+//! UPDATE TASK BY ID
+router.put("/update/:id", async (req, res) => {
+  const task = {
+    description: req.body.description,
+  };
 
-    const query = { where: { id: req.params.id }}
+  const query = { where: { id: req.params.id } };
 
-    TaskModel.update(task, query)
-    .then(list => res.status(200).json({message: "Your task has been updated!", list}))
-    .catch(err => res.status(500).json({error: err}))
+  TaskModel.update(task, query)
+    .then((list) =>
+      res.status(200).json({ message: "Your task has been updated!", list })
+    )
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
+//! DELETE TASK BY ID
 router.delete("/delete/:id", (req, res) => {
-    const query = { where: { id: req.params.id } };
-  
-    TaskModel.destroy(query)
-      .then((task) => res.status(200).json({ message: "Yeet that Task!" }))
-      .catch((err) => res.status(500).json({ error: err }));
-  });
+  const query = { where: { id: req.params.id } };
+
+  TaskModel.destroy(query)
+    .then((task) => res.status(200).json({ message: "Yeet that Task!" }))
+    .catch((err) => res.status(500).json({ error: err }));
+});
 
 module.exports = router;
