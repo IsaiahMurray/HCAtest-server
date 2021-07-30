83d1d8ec0e5e2173e10627a4bc1cb61c88775b13
Isaiah Murray <murray.isaiah13@gmail.com>
2021-07-29 21:15:39 -0400

    Updated list update and delete

--- controllers/listcontroller.js
+++ controllers/listcontroller.js
@@ -37,6 +37,8 @@ router.get("/", async (req, res) => {
   try {
     const allLists = await ListModel.findAll({
       where: { owner: req.user.id },
+      include: [{ model: TaskModel, as: "tasks" }],
+      required: true,
     });
 
     if (allLists.length === 0 || null) {
@@ -56,47 +58,12 @@ router.get("/", async (req, res) => {
   }
 });
 
-//! GET ALL TASKS WITH SINGLE CORRESPONDING LIST BY ID
-router.get("/tasklist/:listId", async (req, res) => {
-  try {
-    const list = await ListModel.findOne({
-      where: { owner: req.user.id },
-    });
-
-    if (list.length === 0 || null) {
-      return res.status(204).json({
-        message: "You do not have any lists yet. Go make some!",
-      });
-    } else {
-      const tasks = await TaskModel.findAll({
-        where: { listId: req.params.listId },
-      });
-      if (tasks.length === 0 || null) {
-        return res.status(200).json({
-          message: "You do not have any tasks for this list yet. Go make some!",
-          list,
-        });
-      } else {
-        return res.status(200).json({
-          message: "Lists and Tasks have successfully been retrieved!",
-          list,
-          tasks,
-        });
-      }
-    }
-  } catch (err) {
-    res.status(500).json({
-      message: `Lists and tasks could not be retrieved: ${err}`,
-    });
-  }
-});
-
 
 //! GET LIST BY TITLE
 router.get("/:title", (req, res) => {
   let title = req.params.title;
 
-  List.findAll({
+  ListModel.findAll({
     where: { title: title },
     include: [{ model: TaskModel, as: "tasks" }],
     required: true,
@@ -108,31 +75,44 @@ router.get("/:title", (req, res) => {
     });
 });
 
-//! GET ALL TASKS WITH CORRESPONDING LIST BY TITLE
 
 //! UPDATE LIST BY ID
-router.put("/update/:id", (req, res) => {
-  const list = {
-    title: req.body.title,
-    description: req.body.description,
-  };
+router.put("/edit/:listId", async (req, res) => {
+  const { title, description } = req.body;
 
-  const query = { where: { id: req.params.id } };
+  try {
+    const updateList = await ListModel.update(
+      { title, description },
+      { where: { id: req.params.listId, owner: req.user.id } }
+    );
 
-  List.update(list, query)
-    .then((list) =>
-      res.status(200).json({ message: "Your list has been updated", list })
-    )
-    .catch((err) => res.status(500).json({ error: err }));
+    res.status(200).json({
+      message: "List has been updated!",
+      listEntry,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Could not update list: ${err}`,
+    });
+  }
 });
 
-//! DELETE LIST BY ID
-router.delete("/delete/:id", (req, res) => {
-  const query = { where: { id: req.params.id } };
 
-  List.destroy(query)
-    .then((lists) => res.status(200).json({ message: "Yeet that List!" }))
-    .catch((err) => res.status(500).json({ error: err }));
+//! DELETE LIST BY ID
+router.delete("/delete/:id", async (req, res) => {
+  const query = { where: { id: req.params.id, owner: req.user.id } };
+
+  try {
+    const destroyedList = await ListModel.destroy(query);
+    res.status(200).json({
+      message: "List has been destroyed!",
+      destroyedList,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Could not destroy list: ${err}`,
+    });
+  }
 });
 
 module.exports = router;
